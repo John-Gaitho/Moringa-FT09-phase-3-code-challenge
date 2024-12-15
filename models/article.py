7846b4d214e2ae3b97d2615580a5060bfb7bdb73
John Gaitho <jgaitho016@gmail.com>
2024-12-15 21:58:42 +0300

    code

--- models/article.py
+++ models/article.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 class Article:
     def __init__(self, id, title, content, author_id, magazine_id):
         self.id = id
@@ -8,3 +10,31 @@ class Article:
 
     def __repr__(self):
         return f"<Article {self.title}>"
+
+    @property
+    def author(self):
+        conn = sqlite3.connect('magazine.db')
+        c = conn.cursor()
+        c.execute("""
+        SELECT authors.name
+        FROM authors
+        JOIN articles ON articles.author_id = authors.id
+        WHERE articles.id = ?
+        """, (self.id,))
+        author = c.fetchone()
+        conn.close()
+        return author if author else None
+
+    @property
+    def magazine(self):
+        conn = sqlite3.connect('magazine.db')
+        c = conn.cursor()
+        c.execute("""
+        SELECT magazines.name
+        FROM magazines
+        JOIN articles ON articles.magazine_id = magazines.id
+        WHERE articles.id = ?
+        """, (self.id,))
+        magazine = c.fetchone()
+        conn.close()
+        return magazine if magazine else None
